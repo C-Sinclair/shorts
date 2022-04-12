36e6d3139819167a0d8ed9c8b5f47e7b0a0227fd
Conor Sinclair <conor@sinclair.software>
2022-04-12 23:12:19 +0100

    fixed asset serving

--- server.mjs
+++ server.mjs
@@ -1,10 +1,14 @@
 import express from 'express'
 import { handler } from './dist/server/entry.mjs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 3000
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
-app.use(express.static('./public'))
+app.use(express.static(__dirname + '/dist/client'))
 app.use(handler);
 
 app.listen(PORT);
--- src/pages/healthcheck.js
+++ src/pages/healthcheck.js
@@ -1,4 +1,4 @@
-export function post(_params, _request) {
+export function get() {
   return new Response(JSON.stringify({ ok: true }), {
     status: 200,
     headers: {
