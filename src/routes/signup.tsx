506ffc74a830101ca8f89303291bb503aa6d2c8a
Conor Sinclair <conor@sinclair.software>
2022-04-17 12:39:20 +0100

    small updates

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -2,11 +2,9 @@ import { AuthForm } from '~/components'
 
 export default function Signup() {
   return (
-
-  <>
-  <h1>Signup</h1>
-  <AuthForm formType='signup' />
+    <>
+      <h1>Signup</h1>
+      <AuthForm formType="signup" />
     </>
   )
 }
-
--- src/routes/upload.tsx
+++ src/routes/upload.tsx
@@ -1,5 +1,13 @@
+import { createResource } from 'solid-js'
 import { isLoggedIn } from '~/auth'
 
-export function Upload() {
+export function routeData(request: Request) {
+  const [allowed] = createResource(() =>
+    isLoggedIn(request.headers, ['creator']),
+  )
+  return allowed
+}
+
+export default function Upload() {
   return <h1>Upload</h1>
 }
