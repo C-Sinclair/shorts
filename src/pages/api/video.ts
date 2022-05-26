d20ce21b9a96d66febb01afccc1ee760461162cb
Conor Sinclair <conor@sinclair.software>
2022-05-26 23:13:48 +0100

    created basic api route for upload of video

---
+++ src/pages/api/video.ts
@@ -0,0 +1,49 @@
+import multiparty from "multiparty";
+import { upload } from "~/server/mux";
+import { createReadStream } from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const stream = createReadStream("upload.mp4");
+
+  await new Promise((resolve, reject) => {
+    const form = new multiparty.Form();
+
+    form.on("part", (part) => {
+      if (part.filename === undefined) {
+        // filename is not defined when this is a field and not a file
+        console.log("got field named " + part.name);
+        // ignore field's content
+        part.resume();
+
+        if (part.filename !== undefined) {
+          // filename is defined when this is a file
+          console.log("got file named " + part.name);
+          stream.push(part.read());
+        }
+
+        part.on("error", (e) => {
+          console.error(e);
+          reject(e);
+        });
+
+        part.on("end", () => {
+          resolve(true);
+        });
+      }
+    });
+
+    form.parse(req);
+  });
+  const meta = await upload(stream);
+  return res.status(200).json(meta);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+    sizeLimit: "500mb",
+  },
+};
+
+export default handler;
--- src/server/mux/index.ts
+++ src/server/mux/index.ts
@@ -1,4 +1,5 @@
 import Mux from "@mux/mux-node";
+import fetch from "node-fetch";
 
 const token = process.env.MUX_ACCESS_TOKEN_ID as string;
 const secret = process.env.MUX_SECRET_KEY as string;
@@ -7,3 +8,18 @@ const secret = process.env.MUX_SECRET_KEY as string;
  * Used for programmatic access of Video assets
  */
 const mux = new Mux(token, secret, {});
+
+const { Video } = mux;
+
+export async function upload(stream: NodeJS.ReadableStream) {
+  try {
+    const upload = await Video.Uploads.create({
+      new_asset_settings: { playback_policy: "public" },
+    });
+    const res = await fetch(upload.url, { method: "PUT", body: stream });
+    console.log(`File uploaded`);
+    return await res.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
